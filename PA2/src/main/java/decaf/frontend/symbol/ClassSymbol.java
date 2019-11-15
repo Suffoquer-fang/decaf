2fb128829947b9287103b54cce4b555fa1255fe8
方言 <2017011462@secoder.net>
2019-11-16 02:43:51 +0800

    simple merge except call func

---
+++ PA2/src/main/java/decaf/frontend/symbol/ClassSymbol.java
@@ -0,0 +1,73 @@
+package decaf.frontend.symbol;
+
+import decaf.frontend.scope.ClassScope;
+import decaf.frontend.scope.GlobalScope;
+import decaf.frontend.tree.Pos;
+import decaf.frontend.type.ClassType;
+
+import java.util.Optional;
+import java.util.TreeSet;
+
+/**
+ * Class symbol, representing a class definition.
+ */
+public final class ClassSymbol extends Symbol {
+
+    public final Optional<ClassSymbol> parentSymbol;
+
+    public final ClassType type;
+
+    /**
+     * Associated class scope of this class.
+     */
+    public final ClassScope scope;
+
+    public ClassSymbol(String name, ClassType type, ClassScope scope, Pos pos) {
+        super(name, type, pos);
+        this.parentSymbol = Optional.empty();
+        this.scope = scope;
+        this.type = type;
+        scope.setOwner(this);
+    }
+
+    public ClassSymbol(String name, ClassSymbol parentSymbol, ClassType type, ClassScope scope, Pos pos) {
+        super(name, type, pos);
+        this.parentSymbol = Optional.of(parentSymbol);
+        this.scope = scope;
+        this.type = type;
+        scope.setOwner(this);
+    }
+
+    @Override
+    public GlobalScope domain() {
+        return (GlobalScope) definedIn;
+    }
+
+    @Override
+    public boolean isClassSymbol() {
+        return true;
+    }
+
+    /**
+     * Set as main class, by {@link decaf.frontend.typecheck.Namer}.
+     */
+    public void setMainClass() {
+        main = true;
+    }
+
+    /**
+     * Is it a main function?
+     *
+     * @return true/false
+     */
+    public boolean isMainClass() {
+        return main;
+    }
+
+    @Override
+    protected String str() {
+        return "class " + name + parentSymbol.map(classSymbol -> " : " + classSymbol.name).orElse("");
+    }
+
+    private boolean main;
+}
